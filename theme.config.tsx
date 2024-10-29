b744e50fad0229fce0ffcc76c9357d7ae287d875
Mikuの鬆 <palomiku@outlook.com>
2024-10-29 23:59:45 +0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,49 +1,21 @@
 import React from 'react'
 import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
 import { useRouter } from 'next/router';
-const logo = (
-  <span>
-    <img
-      height={40}
-      src="../logo.png"
-    />
-    <span>Sugarless</span>
 
-    <style jsx>{`
-      span {
-        padding: 0.5rem 0.5rem 0.5rem 0;
-        mask-image: linear-gradient(
-          60deg,
-          black 25%,
-          rgba(0, 0, 0, 0.2) 50%,
-          black 75%
-        );
-        mask-size: 400%;
-        mask-position: 0%;
-        display: flex;
-        align-items: center;
-        font-size: 16px;
-        font-weight: 600;
-      }
-      img {
-        margin-right: 8px;
-        height: 40px;
-      }
-      span:hover {
-        mask-position: 100%;
-        transition: mask-position 1s ease, -webkit-mask-position 1s ease;
-      }
-    `}</style>
-  </span>
-)
-const config: DocsThemeConfig = {
-  logo,
+interface CustomDocsThemeConfig extends DocsThemeConfig {
+  frontMatter?: {
+    description?: string;
+  };
+}
+
+const config: CustomDocsThemeConfig = {
+  // ... existing code ...
   head: function useHead() {
     const {title} = useConfig()
     const image = 'https://docs.i-hentai.cc/logo.png'
 
     const description =
-    (config.frontMatter &&  config.frontMatter.description) ||
+      (config.frontMatter && config.frontMatter.description) ||
       'Sugarless是一个前后端分离社区系统，基于Nuxt.js'
     return (
       <>
