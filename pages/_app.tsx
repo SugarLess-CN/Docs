5f7c91ef7cd8af3689ad6aef59e38270ad4dfbad
Mikuの鬆 <palomiku@outlook.com>
2024-10-30 20:20:02 +0800

    增删组件与更新依赖

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,7 @@
-import { OpenpanelProvider } from '@openpanel/nextjs'
 import '../public/style.css'
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <OpenpanelProvider
-        url="https://api.openpanel.dev"
-        clientId="feee95af-b002-4b49-a90f-2d6b9c7b7e17"
-        trackScreenViews={true}
-        trackOutgoingLinks={true}
-      />
-
       <Component {...pageProps} />
-    </>
   )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,23 +7,6 @@ export default class MyDocument extends Document {
       <Html lang="zh-cn">
         <Head>
           <meta charSet="UTF-8" />
-
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-
-          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "lsoepndf05");`,
-            }}
-          ></script>
         </Head>
 
         <body>
